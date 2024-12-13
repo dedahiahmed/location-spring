9026e013a7e1517980e413c03c56e5d30c296004
dedahiahmed <dedahiahmed10@gmail.com>
2024-12-13 09:21:35 -0500

    Add authorization header in swagger

--- src/main/java/mr/iscae/configs/OpenApiConfig.java
+++ src/main/java/mr/iscae/configs/OpenApiConfig.java
@@ -2,9 +2,13 @@ package mr.iscae.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.Operation;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
 @Configuration
 public class OpenApiConfig {
@@ -23,5 +27,20 @@ public class OpenApiConfig {
 
         return new OpenAPI()
                 .addServersItem(server);
+    }
+
+    @Bean
+    public OperationCustomizer customGlobalHeaders() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            Parameter authHeader = new Parameter()
+                    .in("header")
+                    .name("Authorization")
+                    .description("Authorization header")
+                    .required(false)
+                    .schema(new io.swagger.v3.oas.models.media.StringSchema());
+
+            operation.addParametersItem(authHeader);
+            return operation;
+        };
     }
 }
